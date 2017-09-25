cedafc3c186f350eea5d33dcd1fecbeeb6cf5588
Michelle Foy <michelle.foy@cenx.com>
2017-09-25 15:58:08 -0400

    Basic implementation

--- queensplace.pl
+++ queensplace.pl
@@ -1,1 +1,13 @@
 % This is a program for Queens Placement
+findSolution([]).
+
+findSolution([X/Y | Others]) :- findSolution(Others),
+                                member(Y, [1, 2, 3, 4, 5, 6, 7, 8]),
+                                doNotAttack(X/Y, Others), !.
+
+doNotAttack(_, []).
+
+doNotAttack(X/Y,[X1/Y1 | Others]) :- Y =\= Y1,
+                                     Y1 - Y =\= X1 - X,
+                                     Y1 - Y =\= X - X1,
+                                     doNotAttack(X/Y, Others).
