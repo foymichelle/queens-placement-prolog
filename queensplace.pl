e03fc7e33fcbd40ca232300d3c5850f544d91da9
Michelle Foy <michelle.foy@cenx.com>
2017-09-25 17:06:30 -0400

    Working implementation - needs testing.

--- queensplace.pl
+++ queensplace.pl
@@ -1,13 +1,13 @@
-% This is a program for Queens Placement
-findSolution([]).
-
-findSolution([X/Y | Others]) :- findSolution(Others),
-                                member(Y, [1, 2, 3, 4, 5, 6, 7, 8]),
-                                doNotAttack(X/Y, Others), !.
+findSolutions([]).
+findSolutions([X/Y | Others]) :- findSolutions(Others),
+                                 member(Y, [1, 2, 3, 4, 5, 6, 7, 8]),
+                                 doNotAttack(X/Y, Others).
 
 doNotAttack(_, []).
-
 doNotAttack(X/Y,[X1/Y1 | Others]) :- Y =\= Y1,
                                      Y1 - Y =\= X1 - X,
                                      Y1 - Y =\= X - X1,
                                      doNotAttack(X/Y, Others).
+
+% To get solutions:
+% findSolutions([1/A,2/B,3/C,4/D,5/E,6/F,7/G,8/H]).
