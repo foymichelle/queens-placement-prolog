e96cf5c0fc9200e9a54ed4a2da3cee71c9f82f8e
Michelle Foy <michelle.foy@cenx.com>
2017-09-25 17:08:25 -0400

    Re-order

--- queensplace.pl
+++ queensplace.pl
@@ -1,7 +1,5 @@
-findSolutions([]).
-findSolutions([X/Y | Others]) :- findSolutions(Others),
-                                 member(Y, [1, 2, 3, 4, 5, 6, 7, 8]),
-                                 doNotAttack(X/Y, Others).
+% To get solutions:
+% findSolutions([1/A,2/B,3/C,4/D,5/E,6/F,7/G,8/H]).
 
 doNotAttack(_, []).
 doNotAttack(X/Y,[X1/Y1 | Others]) :- Y =\= Y1,
@@ -9,5 +7,7 @@ doNotAttack(X/Y,[X1/Y1 | Others]) :- Y =\= Y1,
                                      Y1 - Y =\= X - X1,
                                      doNotAttack(X/Y, Others).
 
-% To get solutions:
-% findSolutions([1/A,2/B,3/C,4/D,5/E,6/F,7/G,8/H]).
+findSolutions([]).
+findSolutions([X/Y | Others]) :- findSolutions(Others),
+                                 member(Y, [1, 2, 3, 4, 5, 6, 7, 8]),
+                                 doNotAttack(X/Y, Others).
